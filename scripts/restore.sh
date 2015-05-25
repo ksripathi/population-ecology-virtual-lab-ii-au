0f039554f3dcf3e174684583095e534c6f4446a4
root <root@localhost.localdomain>
2015-05-25 22:06:37 +0530

    files added for level 6

---
+++ scripts/restore.sh
@@ -0,0 +1,10 @@
+# !/bin/bash
+# restore script for Population Ecology lab II
+
+# restore script restores the backup of the data of the Population Ecology lab II
+ 
+# Since there is no data being used in this lab , the script does nothing.
+
+# Usage of the script, type
+
+# sh scripts/restore.sh
